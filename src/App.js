68725b5e8f772f6156f92e2fb0ca283edd6f6bfe
ben foley <ben@cbmm.io>
2019-01-23 17:30:56 +1000

    drop files

--- src/App.js
+++ src/App.js
@@ -1,28 +1,50 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+import classNames from "classnames";
+import Dropzone from "react-dropzone";
 
 class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-      </div>
-    );
-  }
+    state = {
+        files: [],
+        fileNames: []
+    };
+
+    onDrop = (acceptedFiles, rejectedFiles) => {
+        console.log(acceptedFiles);
+        const fileNames = acceptedFiles.map(f => f.name);
+        this.setState({ ...this.state, files: acceptedFiles, fileNames: fileNames  });
+    };
+
+    render() {
+        const fileNameList = (this.state.fileNames) ? (this.state.fileNames.map((f) => <li key={f}>{f}</li>)) : ''
+
+        return (
+            <div className="App">
+                <Dropzone className="dropzone"  onDrop={this.onDrop}>
+                    {({ getRootProps, getInputProps, isDragActive }) => {
+                        return (
+                            <div
+                                {...getRootProps()}
+                                className={classNames("dropzone", {
+                                    "active": isDragActive
+                                })}
+                            >
+                                <input {...getInputProps()} />
+                                {isDragActive ? (
+                                    <p>Drop files here...</p>
+                                ) : (
+                                    <p>
+                                        Try dropping some files here, or click
+                                        to select files to upload.
+                                    </p>
+                                )}
+                            </div>
+                        );
+                    }}
+                </Dropzone>
+                <ul>{fileNameList}</ul>
+            </div>
+        );
+    }
 }
 
 export default App;
